83f25bc3837996122c22120e8179e038d2ec9e48
Francesc Martínez <francescmm@gmail.com>
2018-08-25 20:34:08 +0200

    Added skip-test example

--- DummyTesting/tst_mainwindowtest.cpp
+++ DummyTesting/tst_mainwindowtest.cpp
@@ -1,64 +1,60 @@
-#include <QtTest>
-#include <QSignalSpy>
 #include <QCoreApplication>
+#include <QSignalSpy>
+#include <QtTest>
 
 // add necessary includes here
-#include <QtWidgets/QMainWindow>
 #include "MainWindow.h"
 #include "ui_MainWindow.h"
+#include <QtWidgets/QMainWindow>
 
 class MainWindowTest : public QObject
 {
     Q_OBJECT
 
-    public:
-        MainWindowTest();
-        ~MainWindowTest();
+  public:
+    MainWindowTest ();
+    ~MainWindowTest ();
 
-    private slots:
-        void initTestCase();
-        void cleanupTestCase();
-        void test_case1();
+  private slots:
+    void initTestCase ();
+    void cleanupTestCase ();
+    void testCorrectLogin ();
+    void testSkipedWithMessage ();
 
-    private:
-        MainWindow mainWindow;
+  private:
+    MainWindow mainWindow;
 };
 
-MainWindowTest::MainWindowTest()
-{
+MainWindowTest::MainWindowTest () {}
 
+MainWindowTest::~MainWindowTest () {}
+
+void MainWindowTest::initTestCase () {}
+
+void MainWindowTest::cleanupTestCase () {}
+
+void MainWindowTest::testCorrectLogin ()
+{
+    QSignalSpy spy (&mainWindow, &MainWindow::signalIsLogged);
+    QSignalSpy spy2 (&mainWindow, &MainWindow::signalLogginFailed);
+
+    QTest::keyClicks (mainWindow.ui->leUsername, "admin");
+    QTest::keyClicks (mainWindow.ui->lePassword, "admin");
+
+    QTest::mouseClick (mainWindow.ui->pbLogin, Qt::LeftButton);
+
+    // QCOMPARE(mainWindow.ui->leUsername->text(), QString("hello world"));
+    QCOMPARE (spy2.count (), 0);
+    QCOMPARE (spy.count (), 1);
 }
 
-MainWindowTest::~MainWindowTest()
+void MainWindowTest::testSkipedWithMessage ()
 {
+    QSKIP ("Test skiped voluntary.");
 
+    // The following content won't be executed
 }
 
-void MainWindowTest::initTestCase()
-{
-
-}
-
-void MainWindowTest::cleanupTestCase()
-{
-
-}
-
-void MainWindowTest::test_case1()
-{
-    QSignalSpy spy(&mainWindow, &MainWindow::signalIsLogged);
-    QSignalSpy spy2(&mainWindow, &MainWindow::signalLogginFailed);
-
-    QTest::keyClicks(mainWindow.ui->leUsername, "admin");
-    QTest::keyClicks(mainWindow.ui->lePassword, "admin");
-
-    QTest::mouseClick(mainWindow.ui->pbLogin, Qt::LeftButton);
-
-    //QCOMPARE(mainWindow.ui->leUsername->text(), QString("hello world"));
-    QCOMPARE(spy2.count(), 0);
-    QCOMPARE(spy.count(), 1);
-}
-
-QTEST_MAIN(MainWindowTest)
+QTEST_MAIN (MainWindowTest)
 
 #include "tst_mainwindowtest.moc"
