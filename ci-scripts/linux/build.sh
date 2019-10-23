4f60482fe5e876e237680fbcafa604acf3b05307
Francesc M <francescmm@gmail.com>
2019-10-23 11:54:57 +0200

    Update build.sh

--- ci-scripts/linux/build.sh
+++ ci-scripts/linux/build.sh
@@ -1,5 +1,5 @@
 wget -O qt5.zip https://github.com/francescmm/ci-utils/releases/download/qt_5.13/qt5.zip;
-unzip -qq qt5.zip -d qt5;
+unzip -qq qt5.zip;
 export QTDIR=$PWD/qt5
 export PATH=$QTDIR/bin:$PATH;
 export QT_PLUGIN_PATH=$PWD/qt5/plugins;
