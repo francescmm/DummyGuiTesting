9f0ade7710d9687587f1bda824afb2e9a8faf45c
Francesc M <francescmm@gmail.com>
2019-10-23 11:07:45 +0200

    Fixing paths

--- ci-scripts/linux/build.sh
+++ ci-scripts/linux/build.sh
@@ -1,12 +1,13 @@
 wget -O qt5.zip https://github.com/francescmm/ci-utils/releases/download/qt_5.13/qt5.zip;
 unzip -qq qt5.zip -d qt5;
-export PATH=`pwd`/qt5/bin:$PATH;
+export QTDIR=`pwd`/qt5
+export PATH=$QTDIR/bin:$PATH;
 export QT_PLUGIN_PATH=`pwd`/qt5/plugins;
 cd DummyGui
-`pwd`/qt5/bin/qmake DummyGui.pro
+$QTDIR/bin/qmake DummyGui.pro
 make -j 2
 cd ..
 cd DummyTesting
-`pwd`/qt5/bin/qmake DummyTesting.pro
+$QTDIR/bin/qmake DummyTesting.pro
 make -j 2
 ./DummyTesting --platform offscreen
