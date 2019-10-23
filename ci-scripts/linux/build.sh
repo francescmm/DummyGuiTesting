f08183e80f71ae4edd35b808a31cfa70533dd03b
Francesc M <francescmm@gmail.com>
2019-10-23 10:50:40 +0200

    Changing Qt version

--- ci-scripts/linux/build.sh
+++ ci-scripts/linux/build.sh
@@ -1,6 +1,7 @@
-QTDIR="/opt/qt513"
-PATH="$QTDIR/bin:$PATH"
-
+wget -q -O qt5.zip https://github.com/francescmm/ci-utils/releases/download/qt_5.13/qt5.zip;
+unzip -qq qt5.zip -d qt5;
+export PATH=`pwd`/qt5/bin:$PATH;
+export QT_PLUGIN_PATH=`pwd`/qt5/plugins;
 cd DummyGui
 qmake DummyGui.pro
 make -j 2
