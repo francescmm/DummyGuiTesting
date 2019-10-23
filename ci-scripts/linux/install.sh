ab9cd3d0cacc280c37abb595c7cc31ec8c25ddca
Francesc M <francescmm@gmail.com>
2019-10-23 11:50:34 +0200

    Avoiding updates

--- ci-scripts/linux/install.sh
+++ ci-scripts/linux/install.sh
@@ -1,9 +1,9 @@
 sudo add-apt-repository --yes ppa:beineri/opt-qt597-trusty
 sudo add-apt-repository --yes ppa:achadwick/mypaint-testing
 sudo apt-add-repository -y ppa:ubuntu-toolchain-r/test
-sudo apt-get update
-sudo apt-get install -y liblzo2-dev liblz4-dev libfreetype6-dev libpng-dev libegl1-mesa-dev libgles2-mesa-dev libsdl2-dev libglew-dev freeglut3-dev qt59script libsuperlu3-dev qt59svg qt59tools qt59multimedia wget libusb-1.0-0-dev libboost-all-dev liblzma-dev libjson-c-dev libmypaint-dev
-sudo apt-get -qq install g++-4.9 libc6-i386 qt59tools qt59charts-no-lgpl
+#sudo apt-get update
+#sudo apt-get install -y liblzo2-dev liblz4-dev libfreetype6-dev libpng-dev libegl1-mesa-dev libgles2-mesa-dev libsdl2-dev libglew-dev freeglut3-dev qt59script libsuperlu3-dev qt59svg qt59tools qt59multimedia wget libusb-1.0-0-dev libboost-all-dev liblzma-dev libjson-c-dev libmypaint-dev
+sudo apt-get -qq install g++-4.9 libc6-i386
 export CXX="g++-4.9 -std=c++14"
 export CC="gcc-4.9"
 export DISPLAY=:0.0
