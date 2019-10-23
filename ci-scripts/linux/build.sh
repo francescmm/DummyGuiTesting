70d3fc73e71f0a96cfc9e6ed7678cfbc0bb72d0b
Francesc M <francescmm@gmail.com>
2019-10-23 10:58:52 +0200

    Making wget explicit and qmaking from the path

--- ci-scripts/linux/build.sh
+++ ci-scripts/linux/build.sh
@@ -1,12 +1,12 @@
-wget -q -O qt5.zip https://github.com/francescmm/ci-utils/releases/download/qt_5.13/qt5.zip;
+wget -O qt5.zip https://github.com/francescmm/ci-utils/releases/download/qt_5.13/qt5.zip;
 unzip -qq qt5.zip -d qt5;
 export PATH=`pwd`/qt5/bin:$PATH;
 export QT_PLUGIN_PATH=`pwd`/qt5/plugins;
 cd DummyGui
-qmake DummyGui.pro
+`pwd`/qt5/bin/qmake DummyGui.pro
 make -j 2
 cd ..
 cd DummyTesting
-qmake DummyTesting.pro
+`pwd`/qt5/bin/qmake DummyTesting.pro
 make -j 2
 ./DummyTesting --platform offscreen
