3fe177175ccf5b469d2e78a55023de339155fec4
Francesc M <francescmm@gmail.com>
2019-10-23 11:28:15 +0200

    Trying to fix PWD

--- ci-scripts/linux/build.sh
+++ ci-scripts/linux/build.sh
@@ -1,8 +1,8 @@
 wget -O qt5.zip https://github.com/francescmm/ci-utils/releases/download/qt_5.13/qt5.zip;
 unzip -qq qt5.zip -d qt5;
-export QTDIR=`pwd`/qt5
+export QTDIR=$PWD/qt5
 export PATH=$QTDIR/bin:$PATH;
-export QT_PLUGIN_PATH=`pwd`/qt5/plugins;
+export QT_PLUGIN_PATH=$PWD/qt5/plugins;
 cd DummyGui
 $QTDIR/bin/qmake DummyGui.pro
 make -j 2
